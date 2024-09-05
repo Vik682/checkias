58e0917ced182ce5b3e61cea42ee7c0455415dc2
vik68 <surajschoolfanclu@gmail.com>
2024-09-05 13:03:58 -0700

    authentication view remain

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from authentication.models import  User, UserToken
 # Register your models here.
+admin.site.register(User)
+admin.site.register(UserToken)
---
+++ authentication/backends.py
@@ -0,0 +1,24 @@
+from .models import  UserToken
+from rest_framework import authentication
+from rest_framework import exceptions
+
+
+class UserAuthenticationBackend(authentication.TokenAuthentication):
+
+    keyword = 'idToken'
+    model = UserToken
+
+    def authenticate_credentials(self, token):
+        try:
+            decoded_token = UserToken.objects.get(key=token)
+        except UserToken.DoesNotExist:
+            raise exceptions.AuthenticationFailed('Invalid token.')
+
+        try:
+            user = decoded_token.user
+
+        except Exception:
+            raise exceptions.AuthenticationFailed('No such user exists')
+
+        # allow users to make API calls only after profile is completed
+        return (user, decoded_token)
