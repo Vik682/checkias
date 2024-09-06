97dbcb6af02d461a31918875c14409caee23c13a
vik68 <surajschoolfanclu@gmail.com>
2024-09-06 13:51:49 -0700

    evaluator profile update

--- user_profile/models.py
+++ user_profile/models.py
@@ -46,11 +46,68 @@ class Coaching(models.Model):
 class Evaluator(models.Model):
   medium=[('english','English'),
           ('hindi','Hindi')]
+  option_sub=[
+    ('agriculture', 'Agriculture'),
+    ('animal husbandry and veterinary science',  'Animal Husbandry and Veterinary Science'),
+    ('anthropology',  'Anthropology'),
+    ('botany',  'Botany'),
+    ('chemistry',  'Chemistry'),
+    ('civil engineering', 'Civil Engineering'),
+    ('commerce & accountancy', 'Commerce & Accountancy'),
+    ('economics', 'Economics'),
+    ('electrical engineering',  'Electrical Engineering'),
+    ('geography', 'Geography'),
+    ('geology', 'Geology'),
+    ('history',  'History'),
+    ('law', 'Law'),
+    ('management',  'Management'),
+    ('mathematics',  'Mathematics'),
+    ('mechanical engineering',  'Mechanical Engineering'),
+    ('medical science', 'Medical Science'),
+    ('philosophy',  'Philosophy'),
+    ('physics',  'Physics'),
+    ('political science & international relations',  'Political Science & International Relations'),
+    ('psychology', 'Psychology'),
+    ('public administration',  'Public Administration'),
+    ('sociology',  'Sociology'),
+    ('statistics', 'Statistics'),
+    ('zoology',  'Zoology'),
+    ('hindi literature',  'Hindi Literature'),
+    ('assamese literature', 'Assamese Literature'),
+    ('bengali literature',  'Bengali Literature'),
+    ('bodo literature',  'Bodo Literature'),
+    ('dogri literature', 'Dogri Literature'),
+    ('gujarati literature',  'Gujarati Literature'),
+    ('kannada literature',  'Kannada Literature'),
+    ('kashmiri literature',  'Kashmiri Literature'),
+    ('konkani literature',  'Konkani Literature'),
+    ('maithili literature',  'Maithili Literature'),
+    ('malayalam literature',  'Malayalam Literature'),
+    ('manipuri literature',  'Manipuri Literature'),
+    ('marathi literature',  'Marathi Literature'),
+    ('nepali literature',  'Nepali Literature'),
+    ('oriya literature',  'Oriya Literature'),
+    ('punjabi literature',  'Punjabi Literature'),
+    ('sanskrit literature',  'Sanskrit Literature'),
+    ('santhali literature',  'Santhali Literature'),
+    ('sindhi literature',  'Sindhi Literature'),
+    ('tamil literature', 'Tamil Literature'),
+    ('telugu literature',  'Telugu Literature'),
+    ('urdu literature',  'Urdu Literature'),
+    ('english literature',  'English Literature')
+]
+
   name = models.CharField(max_length=30,blank=True)
-  phone_number = models.CharField(max_length=20,blank=True)
+  phone_number = models.IntegerField(max_length=11,blank=True,default=0)
   date_of_birth = models.DateField(null = True, blank = True)
+  Num_of_Prelims=models.IntegerField(max_length=2,blank=True,default=0)
+  Num_of_Mains=models.IntegerField(max_length=2,blank=True,default=0)
+  Num_of_Interviews=models.IntegerField(max_length=2,blank=True,default=0)
   medium = models.CharField(max_length=50,choices=medium,default='english')
   profile_image = models.ImageField(upload_to='profile_images/',blank=True)
+  optional=models.CharField(max_length=90,choices=option_sub,default='Not Selected')
+  rank_secured=models.BooleanField(default=False)
+  
 
   def __str__(self):
     return self.name
